5003ecdff03012b7d17a372d87fcabe987b361ec
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-01-05 15:58:59 +0800

    路由封装

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,22 +2,14 @@ import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } 
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", redirect: "/home" },
-  {
-    path: "/home",
-    name: "Home",
-    component: () => import("@/views/home/index.vue"),
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: () => import("@/views/login/index.vue"),
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () => import("@/views/about/index.tsx"),
-  },
 ];
+
+const modules = import.meta.glob("./modules/*.ts", { eager: true });
+for (const path in modules) {
+  const module = modules[path];
+  // console.log('module', module)
+  routes.push(...(module.default as RouteRecordRaw[]));
+}
 
 export default createRouter({
   history: import.meta.env.VITE_ROUTER_MODE === "hash" ? createWebHashHistory() : createWebHistory(),
---
+++ src/router/modules/baseRoutes.ts
@@ -0,0 +1,21 @@
+import { RouteRecordRaw } from "vue-router";
+
+const baseRoutes: RouteRecordRaw[] = [
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import("@/views/home/index.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("@/views/login/index.vue"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () => import("@/views/about/index.tsx"),
+  },
+];
+
+export default baseRoutes;
