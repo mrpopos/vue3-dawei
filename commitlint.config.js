c6670daf979e9c53a23b0de776dd33faaaed85a8
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-01-06 17:18:37 +0800

    git规范提交信息

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', 'ci', 'perf', 'build', 'wip'],
+    ],
+    'scope-empty': [2, 'never'], // scope 不可为空
+    'subject-case': [2, 'never', ['start-case', 'pascal-case']], // subject 不允许使用大写开头
+    'header-max-length': [2, 'always', 72], // 限制提交信息的长度
+  },
+}
