054974467505de126b773319b42ebea35acefc2d
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-01-06 21:25:46 +0800

    feat(review): axios

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,21 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert', 'ci', 'perf', 'build', 'wip', 'frame'],
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'revert',
+        'ci',
+        'perf',
+        'build',
+        'wip',
+        'frame',
+      ],
     ],
     'scope-empty': [2, 'never'], // scope 不可为空
     'subject-case': [2, 'never', ['start-case', 'pascal-case']], // subject 不允许使用大写开头
