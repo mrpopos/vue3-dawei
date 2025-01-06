f398afe13f637cdfa7fc6893209093722cb756c3
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-01-06 16:36:28 +0800

    代码提交规范test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,4 @@
 module.exports = {
-  /*
-   * Resolve and load @commitlint/config-conventional from node_modules.
-   * Referenced packages must be installed
-   */
-  extends: ['@commitlint/config-conventional'],
-  /*
-   * Resolve and load conventional-changelog-atom from node_modules.
-   * Referenced packages must be installed
-   */
-  parserPreset: 'conventional-changelog-atom',
-  /*
-   * Resolve and load @commitlint/format from node_modules.
-   * Referenced package must be installed
-   */
-  formatter: '@commitlint/format',
-  /*
-   * Custom URL to show upon failure
-   */
-  helpUrl:
-    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
   // 提交规则
   rules: {
     // 提交的type必须是以下之一：'fix', 'feat', 'chore', 'docs', 'style', 'refactor', 'perf', 'test'
@@ -35,5 +15,12 @@ module.exports = {
     'subject-full-stop': [0, 'never'],
     // 提交的subject必须是小写
     'subject-case': [0, 'never'],
+  },
+  // 提交的message必须符合Angular风格
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.*): (.*)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
   },
 }
