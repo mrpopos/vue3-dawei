1fefd4cac9c0eb2916ede8f0163f48e7b465d4ea
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-01-06 16:55:53 +0800

    git规范提交信息

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,30 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    'lerna', // prefixed with commitlint-config-*,
+    '@commitlint/config-conventional', // scoped packages are not prefixed
+  ],
+  // 提交规则
+  rules: {
+    // 提交的type必须是以下之一：'fix', 'feat', 'chore', 'docs', 'style', 'refactor', 'perf', 'test'
+    'type-enum': [
+      2,
+      'always',
+      ['fix', 'feat', 'chore', 'docs', 'style', 'refactor', 'perf', 'test'],
+    ],
+    // 提交的scope必须是字符串
+    'scope-enum': [2, 'always', []],
+    // 提交的subject不能为空
+    'subject-empty': [2, 'never'],
+    // 提交的subject必须是50个字符以内
+    'subject-full-stop': [0, 'never'],
+    // 提交的subject必须是小写
+    'subject-case': [0, 'never'],
+  },
+  // 提交的message必须符合Angular风格
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.*): (.*)$/,
+      headerCorrespondence: ['type', 'subject'],
+    },
+  },
 }
