0f4d8b80db84bef93da11c6179087569ee20aecb
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-01-08 23:27:40 +0800

    feat(page): user page

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -46,6 +46,17 @@
         min-width="100"
       />
     </el-table>
+    <!-- 分页 -->
+    <el-row justify="space-evenly" class="pagination">
+      <el-pagination
+        background
+        layout="sizes, prev, pager, next, jumper"
+        :total="total"
+        :page-sizes="[5, 10, 20, 50]"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </el-row>
   </div>
 </template>
 
@@ -63,13 +74,31 @@ const queryParams = reactive({
   pageNum: 1,
   pageSize: 10,
 })
+const total = ref<number>()
 
 async function getTableData() {
   const res = await page(queryParams)
   tableData.value = res.data.list
+  total.value = res.data.total
+}
+
+function handleSizeChange(val: number) {
+  queryParams.pageSize = val
+  getTableData()
+}
+
+function handleCurrentChange(val: number) {
+  queryParams.pageNum = val
+  getTableData()
 }
 
 onMounted(() => {
   getTableData()
 })
 </script>
+
+<style lang="less" scoped>
+.pagination {
+  margin-top: 20px;
+}
+</style>
