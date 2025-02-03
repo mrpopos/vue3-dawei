ade04c0615939b46a754320dda23887630cbc41f
mrpopos-wolton <mrpopos.wolton@gmail.com>
2025-02-03 21:56:57 +0800

    feat(test): test cicd

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,12 @@
 <template>
   <!-- 开启顶部安全区适配 -->
   <van-nav-bar safe-area-inset-top />
-  <Layout />
+  <!-- <Layout /> -->
+  <h1>hello world</h1>
   <!-- 开启底部安全区适配 -->
   <van-number-keyboard safe-area-inset-bottom />
 </template>
 
 <script setup lang="ts">
-import Layout from '@/layout/index.vue'
+// import Layout from '@/layout/index.vue'
 </script>
